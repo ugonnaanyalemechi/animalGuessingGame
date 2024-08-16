0ec92426c6bd88903047f4f554eb0c1076b6005d
ua <anyugonna@gmail.com>
2024-08-15 22:32:49 -0500

    style: address minor errors in code structure, implementation, and output

--- GameSave.cpp
+++ GameSave.cpp
@@ -5,30 +5,29 @@
 
 using namespace std;
 
-ofstream outputFile;
 Debugging debug3;
 
 void GameSave::saveAnimalGameData(AnimalNode* rootNode, GameSetup gameSetup) {
-    ofstream outputFile(gameSetup.animalFileName_SAVE);
+    ofstream outputFile(gameSetup.gameDataFile);
     checkOutputFileOpens(outputFile, gameSetup);
     writeGameDataToFile(outputFile, rootNode);
 }
 
 void GameSave::checkOutputFileOpens(ostream& outputFile, GameSetup gameSetup) {
     if (outputFile.fail())
-        declareFileOpenFail(gameSetup.animalFileName_SAVE);
+        declareFileOpenFail(gameSetup.gameDataFile);
 }
 
 void GameSave::writeGameDataToFile(ostream& outputFile, AnimalNode* currentNode) { // using pre-order traversal
     if (currentNode->animal != "") { // if an animal guess node
-        outputFile << "G" << endl;
-        outputFile << currentNode->animal << endl;
+        outputFile << "G\n";
+        outputFile << currentNode->animal << "\n";
         debug3.show("Animal guess saved", currentNode->animal);
         debug3.pause();
     }
     else { // else, it is a question node
-        outputFile << "Q" << endl;
-        outputFile << currentNode->question << endl;
+        outputFile << "Q\n";
+        outputFile << currentNode->question << "\n";
         debug3.show("Question saved", currentNode->question);
         debug3.pause();
         writeGameDataToFile(outputFile, currentNode->yesAns);
